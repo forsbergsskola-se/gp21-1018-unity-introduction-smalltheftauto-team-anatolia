cff32c51dd949057a26f279103b33462da9e9d27
Muhammed Al <muhammed.al-idani@student.forsbergsskola.se>
2021-10-20 09:59:13 +0200

    Update CarMovmentMA.cs

--- projects/SmallTheftAuto/Assets/M.A/Script/CarMovmentMA.cs
+++ projects/SmallTheftAuto/Assets/M.A/Script/CarMovmentMA.cs
@@ -4,8 +4,9 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class CarMovmentMA : MonoBehaviour
-{       
-    
+{
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,16 +16,19 @@ public class CarMovmentMA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        transform.Translate(5f*Time.deltaTime*Input.GetAxis("Vertical"), 0f, 0f);
+        transform.Translate(-5f*Time.deltaTime*Input.GetAxis("Horizontal"),0f,0f);
+        
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-0.1f,0f,0f);
-            
+            transform.Rotate(0f, 0f, 180f*Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(0.1f,0f,0f);
+            transform.Rotate(0f, 0f, 180f*Time.deltaTime);
         }
+        
     }
 }
 
