0defd695e35afdb7cd805a4cbce4eacc034b1c03
Muhammed Al <muhammed.al-idani@student.forsbergsskola.se>
2021-10-22 10:52:33 +0200

    PlayerHealth+HealthBar

--- projects/SmallTheftAuto/Assets/M.A/Script/PlayerMovmentMA.cs
+++ projects/SmallTheftAuto/Assets/M.A/Script/PlayerMovmentMA.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class PlayerMovmentMA : MonoBehaviour
 {
+    public HealthbarBehaviourMA Healthbar;
+    public float Hitpoints;
+    public float MaxHitpoints = 5;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints,MaxHitpoints);
+    }
+
+    public void TakeHit(float damage)
+    {
+        Hitpoints -= damage;
+        if (Hitpoints<=0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
