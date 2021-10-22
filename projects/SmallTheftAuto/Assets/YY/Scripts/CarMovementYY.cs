37751716324ff81e57c9be92ce8f644efc5ef363
Yasin Yekta <yasin.cakiroglu@student.forsbergsskola.se>
2021-10-22 12:30:17 +0200

    I dont know  what changed

---
+++ projects/SmallTheftAuto/Assets/YY/Scripts/CarMovementYY.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CarMovementYY : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(0f, 5f*Time.deltaTime*Input.GetAxis("Vertical"), 0f);
+        transform.Translate(0f,-5f*Time.deltaTime*Input.GetAxis("Horizontal"),0f);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, 0f, 180f*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f,0f,-180f*Time.deltaTime);
+        }
+    }
+}
