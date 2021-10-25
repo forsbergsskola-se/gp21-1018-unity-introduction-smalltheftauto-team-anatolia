705ad03cf67f2e0259ae2b31d1de4e1da464eb23
Muhammed Al <muhammed.al-idani@student.forsbergsskola.se>
2021-10-25 13:52:00 +0200

    prefab bullet+Tank Shooting

---
+++ projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/Shooting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet =Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb= bullet.GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.up*bulletForce,ForceMode.Impulse);
+    }
+}
