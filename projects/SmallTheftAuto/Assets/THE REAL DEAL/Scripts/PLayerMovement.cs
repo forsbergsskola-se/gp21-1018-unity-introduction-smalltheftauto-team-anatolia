ed39d4fb8b8cfe68b762a2fb12ab201d8eedba08
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-10-26 15:38:15 +0200

    Player can move and rotate now

---
+++ projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/PLayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, -5f*Time.deltaTime*Input.GetAxis("Vertical"), 0f);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f,0f,-180f*Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, 180f * Time.deltaTime);
+        }
+    }
+}
