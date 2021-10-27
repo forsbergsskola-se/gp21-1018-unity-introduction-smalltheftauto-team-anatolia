5c15aae790f2e64387190eae821a12128725e628
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-10-27 13:17:26 +0200

    tried to put animation to player (didnt work)

---
+++ projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/EnemyDamage.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+   public float damage = 20f;
+   
+}
--- projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/PLayerMovement.cs
+++ projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/PLayerMovement.cs
@@ -1,23 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PLayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+<<<<<<< HEAD
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
     }
+=======
+    public int points = 0;
+    public bool isHitting;
+    
+>>>>>>> d9f41b07b7928f00982a37735513afe263a7c109
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f, -5f*Time.deltaTime*Input.GetAxis("Vertical"), 0f);
+<<<<<<< HEAD
+        // if player presses W  key
+        if (Input.GetKey(KeyCode.W))
+        {
+            // then set the isWalking boolean to be true
+            animator.SetBool("isWalking", true);
+        }
+=======
+        transform.Translate(0f, -5f * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
+>>>>>>> d9f41b07b7928f00982a37735513afe263a7c109
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0f,0f,-180f*Time.deltaTime);
+            transform.Rotate(0f, 0f, -180f * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
@@ -25,4 +42,23 @@ public class PLayerMovement : MonoBehaviour
             transform.Rotate(0f, 0f, 180f * Time.deltaTime);
         }
     }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(-256, 172, 160, 30), "Coin : " + points);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="Enemy")
+        {
+            isHitting = true;
+        }
+        else
+        {
+            isHitting = false;
+        }
+    }
 }
+
+
