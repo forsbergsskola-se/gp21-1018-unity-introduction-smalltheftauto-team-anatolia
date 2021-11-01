1f42522d1433265b031b62a4f45bb435e39c5e9a
Muhammed Al <muhammed.al-idani@student.forsbergsskola.se>
2021-11-01 14:41:20 +0100

    parkingspot

--- projects/SmallTheftAuto/Assets/M.A/Script/ParkingSpotMA.cs
+++ projects/SmallTheftAuto/Assets/M.A/Script/ParkingSpotMA.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovment : MonoBehaviour
+public class ParkingSpotMA : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool hasCar;
+
+    public GameObject carPrefab;
+    
+    
+    
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
