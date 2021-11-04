cc90bfcd177b88ccafc5ad6b318e9cd5ae6bb077
Muhammed Al <muhammed.al-idani@student.forsbergsskola.se>
2021-11-04 09:22:00 +0100

    clean some unnecessary scripts!

--- projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/CarMovement.cs
+++ projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/CarMovement.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    
     void Start()
-    {
-        
-    }
-
+        {
+            
+        }
     
-    void Update()
-    {
-        transform.Translate(0f, 0f, 180f * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
-        transform.Translate(0f,-5*Time.deltaTime*Input.GetAxis("Horizontal"),0f);
-        if (Input.GetKey(KeyCode.A))
+        
+        void Update()
         {
-            transform.Rotate(0f,0f,180f*Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0f,0f,-180f*Time.deltaTime);
+            transform.Translate(0f, 0f, 180f * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
+            transform.Translate(0f,-5*Time.deltaTime*Input.GetAxis("Horizontal"),0f);
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0f,0f,180f*Time.deltaTime);
+            }
+    
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0f,0f,-180f*Time.deltaTime);
+            }
         }
     }
-}
--- projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/EnemyDamage.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyDamage : MonoBehaviour
-{
-   public float damage = 20f;
-   
-}
