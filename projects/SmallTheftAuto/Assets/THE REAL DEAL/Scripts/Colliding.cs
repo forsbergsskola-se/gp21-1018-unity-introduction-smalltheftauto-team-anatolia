4a2b10dc899bd4c93977ddf9bc3685821be0d51c
Yasin Yekta <yasin.cakiroglu@student.forsbergsskola.se>
2021-11-04 15:40:15 +0100

    Colliding script

---
+++ projects/SmallTheftAuto/Assets/THE REAL DEAL/Scripts/Colliding.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Colliding : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("NewTheftAuto");
+        }
+    }
+}
